8f66e615bbad4b5e0e7727041e662cb054ef9d7b
YiTingTsai <f920826@gmail.com>
2024-09-13 14:22:56 +0800

    麵包屑還是壞

--- template/src/components/AppSidebar.js
+++ template/src/components/AppSidebar.js
@@ -27,7 +27,7 @@ const AppSidebar = () => {
   return (
     <CSidebar
       className="border-end"
-      style={{ backgroundColor: 'white' }}
+      colorScheme="light"
       position="fixed"
       unfoldable={unfoldable}
       visible={sidebarShow}
