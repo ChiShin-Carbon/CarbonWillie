a40725dc5ec0c53004cf154d0701179f5d7e5842
HsinJouChen <chen901105@gmail.com>
2024-09-28 21:04:20 +0800

    表格修改

--- template/src/views/theme/search/盤查結果查詢.js
+++ template/src/views/theme/search/盤查結果查詢.js
@@ -19,6 +19,7 @@ import {
 // import { freeSet } from '@coreui/icons'
 // import { getIconsView } from '../brands/Brands.js'
 
+
 const Tabs = () => {
     return (
         <CRow>
@@ -68,33 +69,111 @@ const Tabs = () => {
                                     </div>
                                 </CCardTitle>
                                 </CRow>
-
+                                <table border='1' style={{width: '700px'}}>
+                                    <thead>
+                                    <tr>
+                                        <th scope="col" style={{width: '30%', textAlign: 'center', verticalAlign: 'middle' }}></th>
+                                        <th scope="col" style={{width: '30%',textAlign: 'center', verticalAlign: 'middle' }}>排放當量<br/>(公噸COe/年)</th>
+                                        <th scope="col" style={{width: '20%', textAlign: 'center', verticalAlign: 'middle' }}>百分比<br/>(%)</th>
+                                        <th scope="col" style={{width: '30%', textAlign: 'center', verticalAlign: 'middle' }}>碳費</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td scope="row">總排放量</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td scope="row">直接排放/範疇一</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td scope="row">間接排放/範疇二</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>其他間接排放/範疇三</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                                <CCardSubtitle className="mb-2 text-body-secondary">Card subtitle</CCardSubtitle>
+                                <CCardText>
+                                    Some quick example text to build on the card title and make up the bulk of the
+                                    card&#39;s content.
+                                </CCardText>
+                            </CCardBody>
+                        </CCard>
+                        <br></br>
+                        <CCard style={{ width: '100%' }}>
+                            <CCardBody>
+                                <CRow>
+                                <CCardTitle>直接排放 / 範疇一
+                                    <CButton color="primary" type="submit" className="mb-3" align="right" style={{backgroundColor: '#CA6AAF', borderColor: '#CA6AAF', color: 'white',width:'120px',height:'50px' }}>
+                                    <b><CIcon icon={cilDataTransferDown} className="me-2" /></b>
+                                    </CButton>
+                                    <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                                        <CButton color="primary"><CIcon icon={cilDataTransferDown} className="me-2" /></CButton>
+                                    </div>
+                                </CCardTitle>
+                                </CRow>
                                 <CTable border='1'>
                                     <CTableHead>
                                     <CTableRow>
-                                        <CTableHeaderCell scope="col">#</CTableHeaderCell>
-                                        <CTableHeaderCell scope="col">Class</CTableHeaderCell>
-                                        <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
-                                        <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+                                        <CTableHeaderCell scope="col" style={{ width: '10%', textAlign: 'center', verticalAlign: 'middle' }}></CTableHeaderCell>
+                                        <CTableHeaderCell scope="col" style={{ width: '30%', textAlign: 'center', verticalAlign: 'middle' }}>設備</CTableHeaderCell>
+                                        <CTableHeaderCell scope="col" style={{ width: '20%', textAlign: 'center', verticalAlign: 'middle' }}>排放當量<br/>(公噸COe/年)</CTableHeaderCell>
+                                        <CTableHeaderCell scope="col" style={{ width: '20%', textAlign: 'center', verticalAlign: 'middle' }}>百分比<br/>(%)</CTableHeaderCell>
+                                        <CTableHeaderCell scope="col" style={{ width: '20%', textAlign: 'center', verticalAlign: 'middle' }}>碳費</CTableHeaderCell>
                                     </CTableRow>
                                     </CTableHead>
                                     <CTableBody>
                                     <CTableRow>
-                                        <CTableHeaderCell scope="row">1</CTableHeaderCell>
+                                        <CTableHeaderCell rowSpan={2} style={{ verticalAlign: 'middle' }}>固定排放源</CTableHeaderCell>
                                         <CTableDataCell>Mark</CTableDataCell>
                                         <CTableDataCell>Otto</CTableDataCell>
                                         <CTableDataCell>@mdo</CTableDataCell>
+                                        <CTableDataCell>@mdo</CTableDataCell>
                                     </CTableRow>
                                     <CTableRow>
-                                        <CTableHeaderCell scope="row">2</CTableHeaderCell>
+                                        <CTableDataCell>Mark</CTableDataCell>
+                                        <CTableDataCell>Otto</CTableDataCell>
+                                        <CTableDataCell>@mdo</CTableDataCell>
+                                        <CTableDataCell>@mdo</CTableDataCell>
+                                    </CTableRow>
+                                    <CTableRow>
+                                        <CTableHeaderCell>移動排放</CTableHeaderCell>
                                         <CTableDataCell>Jacob</CTableDataCell>
                                         <CTableDataCell>Thornton</CTableDataCell>
                                         <CTableDataCell>@fat</CTableDataCell>
+                                        <CTableDataCell>@mdo</CTableDataCell>
                                     </CTableRow>
                                     <CTableRow>
-                                        <CTableHeaderCell scope="row">3</CTableHeaderCell>
-                                        <CTableDataCell colSpan={2}>Larry the Bird</CTableDataCell>
+                                        <CTableHeaderCell rowSpan={3}>逸散排放</CTableHeaderCell>
+                                        <CTableDataCell>Larry the Bird</CTableDataCell>
                                         <CTableDataCell>@twitter</CTableDataCell>
+                                        <CTableDataCell>@mdo</CTableDataCell>
+                                        <CTableDataCell>@mdo</CTableDataCell>
+                                    </CTableRow>
+                                    <CTableRow>
+                                        <CTableDataCell>Larry the Bird</CTableDataCell>
+                                        <CTableDataCell>@twitter</CTableDataCell>
+                                        <CTableDataCell>@mdo</CTableDataCell>
+                                        <CTableDataCell>@mdo</CTableDataCell>
+                                    </CTableRow>
+                                    <CTableRow>
+                                        <CTableDataCell>Larry the Bird</CTableDataCell>
+                                        <CTableDataCell>@twitter</CTableDataCell>
+                                        <CTableDataCell>@mdo</CTableDataCell>
+                                        <CTableDataCell>@mdo</CTableDataCell>
                                     </CTableRow>
                                     </CTableBody>
                                     </CTable>
@@ -106,164 +185,7 @@ const Tabs = () => {
                                 </CCardText>
                             </CCardBody>
                         </CCard>
-                        <CForm>
-                        <CRow className="mb-3">
-                            <CCol sm={9}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="account">機構名稱</CFormLabel>
-                                <CFormInput
-                                type="name"
-                                id="name"
-                                placeholder="啟新醫事檢驗所"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                        </CRow>
-                        <CRow className="mb-3">
-                            <CCol sm={4}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="account">管制編號</CFormLabel>
-                                <CFormInput
-                                type="registrationno"
-                                id="registrationno"
-                                placeholder="A39B6572"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                            <CCol sm={1}></CCol>
-                            <CCol sm={4}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="email">核准字號</CFormLabel>
-                                <CFormInput
-                                type="permitno"
-                                id="permitno"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                        </CRow>
-                        <CRow className="mb-3">
-                            <CCol sm={3}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="account">縣市別</CFormLabel>
-                                <CFormInput
-                                type="country"
-                                id="country"
-                                placeholder="台北市"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                            <CCol sm={3}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="email">鄉鎮別</CFormLabel>
-                                <CFormInput
-                                type="township"
-                                id="township"
-                                placeholder="中山區"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                            <CCol sm={3}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="email">里別</CFormLabel>
-                                <CFormInput
-                                type="village"
-                                id="village"
-                                placeholder="行政里"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                        </CRow>
-                        <CRow className="mb-3">
-                            <CCol sm={6}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="account">地址</CFormLabel>
-                                <CFormInput
-                                type="address"
-                                id="address"
-                                placeholder="台北市中山區建國北路三段42號5樓"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                            <CCol sm={3}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="email">郵遞區號</CFormLabel>
-                                <CFormInput
-                                type="postal_code"
-                                id="postal_code"
-                                placeholder="10482"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                        </CRow>
-                        <CRow className="mb-3">
-                            <CCol sm={4}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="account">負責人姓名</CFormLabel>
-                                <CFormInput
-                                type="head"
-                                id="head"
-                                placeholder="楊文仁"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                            <CCol sm={1}></CCol>
-                            <CCol sm={4}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="email">電話</CFormLabel>
-                                <CFormInput
-                                type="telephone"
-                                id="telephone"
-                                placeholder="02-25070723"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                        </CRow>
-                        <CRow className="mb-3">
-                            <CCol sm={4}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="account">電子信箱</CFormLabel>
-                                <CFormInput
-                                type="email"
-                                id="email"
-                                placeholder="name@example.com"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                            <CCol sm={1}></CCol>
-                            <CCol sm={4}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="email">手機</CFormLabel>
-                                <CFormInput
-                                type="mobile"
-                                id="mobile"
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                        </CRow>
-                        <CRow className="mb-3">
-                            <CCol sm={4}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="account">行業名稱</CFormLabel>
-                                <CFormInput
-                                type="industry_name"
-                                id="industry_name"
-                                placeholder='醫學檢驗業'
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                            <CCol sm={1}></CCol>
-                            <CCol sm={4}>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="email">行業代碼</CFormLabel>
-                                <CFormInput
-                                type="industry_code"
-                                id="industry_code"
-                                placeholder='8691'
-                                disabled readOnly/>
-                            </div>
-                            </CCol>
-                        </CRow>
-                        </CForm>
+                        <br></br>
                     </CCardBody>
                 </CCard>
                 </CCardBody>
