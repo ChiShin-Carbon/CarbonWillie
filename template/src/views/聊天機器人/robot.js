1e4e92531b35ae69b99cc60aa7ddad18e3d59723
YiTingTsai <melody920826@gmail.com>
2024-10-19 19:07:17 +0800

    robot暫時完

--- template/src/views/聊天機器人/robot.js
+++ template/src/views/聊天機器人/robot.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import styles from '../../scss/聊天機器人.module.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRobot,faLocationArrow } from '@fortawesome/free-solid-svg-icons';
+import { faRobot, faLocationArrow } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Robot() {
@@ -20,12 +20,46 @@ export default function Robot() {
                         <div className={styles.chatHeader}>
                             <h6>AI機器人-碳智郎</h6>
                         </div>
+
+
                         <div className={styles.chatContent}>
-                            聊天內容
+                            <div className={styles.messageContainer}>
+                                <div className={styles.head}><FontAwesomeIcon icon={faRobot} /></div>
+                                <div className={styles.message}>碳智郎歡迎您~請問要問什麼呢?</div>
+                            </div>
+                            <div className={styles.messageContainer}>
+                                <div className={styles.head}><FontAwesomeIcon icon={faRobot} /></div>
+                                <div className={styles.message}>
+                                    以下是常見問題，也可以輸入訊息獲得回覆
+                                    <div className={styles.faq}>
+                                        <button>碳盤查 vs 碳足跡</button>
+                                        <button>碳盤查範疇</button>
+                                        <button>碳盤查流程</button>
+                                        <button>碳盤查原則</button>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div className={styles.messageContainer}>
+                                <div className={styles.head}><FontAwesomeIcon icon={faRobot} /></div>
+                                <div className={styles.message}>
+                                    以下是常見問題，也可以輸入訊息獲得回覆
+                                    <div className={styles.faq}>
+                                        <button>碳盤查 vs 碳足跡</button>
+                                        <button>碳盤查範疇</button>
+                                        <button>碳盤查流程</button>
+                                        <button>碳盤查原則</button>
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
+
+
                         <div className={styles.chatFooter}>
                             <form>
-                                <input type='text' placeholder='請輸入訊息'/>
+                                <input type='text' placeholder='請輸入訊息' />
                                 <button type='submit'><FontAwesomeIcon icon={faLocationArrow} /></button>
                             </form>
                         </div>
