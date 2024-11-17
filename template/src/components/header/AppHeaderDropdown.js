278a7b9136827341ce6e8f8923e734eed2857b48
Cindy Wang <youhuawang2127cc@gmail.com>
2024-11-17 20:28:45 +0800

    登入後點頭像顯示資料

--- template/src/components/header/AppHeaderDropdown.js
+++ template/src/components/header/AppHeaderDropdown.js
@@ -19,7 +19,7 @@ import {
   cilSettings,
   cilTask,
   cilUser,
-  cilAccountLogout
+  cilAccountLogout,
 } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
@@ -27,23 +27,149 @@ import styles from '/src/scss/雜區.module.css'
 
 import profile from './../../assets/images/avatars/profile.jpg'
 
+import { useEffect, useState } from 'react'
+
 const AppHeaderDropdown = () => {
+  const [org_name, setOrgName] = useState('')
+  const [name, setName] = useState('')
+  const [departmentID, setDepartmentID] = useState('')
+  const [department, setDepartment] = useState('')
+  const [positionID, setPositionID] = useState('')
+  const [position, setPosition] = useState('')
+  const [role, setRole] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(!!window.sessionStorage.getItem('user_id'))
+
+  const getcompanyinfo = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/companyinfo', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ business_id: '00993654' }),
+      })
+      const data = await response.json()
+      console.log(data)
+      if (response.ok) {
+        console.log(data)
+        setOrgName(data.company.org_name)
+      } else {
+        console.log(response.status)
+      }
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
+
+  const getuserinfo = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/userinfo', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: window.sessionStorage.getItem('user_id'),
+        }),
+      })
+      const data = await response.json()
+      console.log(data)
+      if (response.ok) {
+        console.log(data)
+        setName(data.user.username)
+        setDepartmentID(data.user.department)
+        setPositionID(data.user.position)
+        setRole(data.user.role)
+      } else {
+        console.log(response.status)
+      }
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
+
+  const setdept = () => {
+    if (departmentID === 1) {
+      setDepartment('管理部門')
+    } else if (departmentID === 2) {
+      setDepartment('資訊部門')
+    } else if (departmentID === 3) {
+      setDepartment('業務部門')
+    } else if (departmentID === 4) {
+      setDepartment('門診部門')
+    } else if (departmentID === 5) {
+      setDepartment('健檢部門')
+    } else if (departmentID === 6) {
+      setDepartment('檢驗部門')
+    } else {
+      setDepartment('其他')
+    }
+  }
+
+  const setposition = () => {
+    if (positionID === 1) {
+      setPosition('總經理')
+    } else if (positionID === 2) {
+      setPosition('副總經理')
+    } else if (positionID === 3) {
+      setPosition('主管')
+    } else if (positionID === 4) {
+      setPosition('副主管')
+    } else if (positionID === 5) {
+      setPosition('組長')
+    } else {
+      setPosition('其他')
+    }
+  }
+
+  useEffect(() => {
+    getcompanyinfo()
+  }, [])
+
+  useEffect(() => {
+    getuserinfo()
+  }, [])
+
+  useEffect(() => {
+    setdept()
+  }, [departmentID])
+
+  useEffect(() => {
+    setposition()
+  }, [positionID])
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      getuserinfo()
+    }
+  }, [isLoggedIn])
+
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
         <CAvatar src={profile} size="md" />
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
-        <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">Account</CDropdownHeader>
-       
-          <div className={styles.profile}>
-            <div>啟新醫事檢驗所</div>
-            <div>資訊部門</div>
-            <div>職位</div>
-            <div>蔡沂庭</div>
-          </div>
+        {isLoggedIn ? (
+          <>
+            <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">
+              Account
+            </CDropdownHeader>
 
-        {/* <CDropdownItem href="#">
+            <div className={styles.profile}>
+              {role ? (
+                <>
+                  <div>{org_name}</div>
+                  <div>{department}</div>
+                  <div>{position}</div>
+                </>
+              ) : (
+                <div>顧問</div>
+              )}
+              <div>{name}</div>
+            </div>
+
+            {/* <CDropdownItem href="#">
           <CIcon icon={cilBell} className="me-2" />
           Updates
           <CBadge color="info" className="ms-2">
@@ -71,12 +197,16 @@ const AppHeaderDropdown = () => {
             42
           </CBadge>
         </CDropdownItem> */}
-        <CDropdownHeader className="bg-body-secondary fw-semibold my-2">Settings</CDropdownHeader>
-        <CDropdownItem href="/#/theme/user_info">
-          <CIcon icon={cilUser} className="me-2" />
-          個人檔案
-        </CDropdownItem>
-        {/* <CDropdownItem href="#">
+
+            <CDropdownHeader className="bg-body-secondary fw-semibold my-2">
+              Settings
+            </CDropdownHeader>
+
+            <CDropdownItem href="/#/theme/user_info">
+              <CIcon icon={cilUser} className="me-2" />
+              個人檔案
+            </CDropdownItem>
+            {/* <CDropdownItem href="#">
           <CIcon icon={cilSettings} className="me-2" />
           Settings
         </CDropdownItem>
@@ -94,11 +224,21 @@ const AppHeaderDropdown = () => {
             42
           </CBadge>
         </CDropdownItem> */}
-        <CDropdownDivider />
-        <CDropdownItem href="#">
-          <CIcon icon={cilAccountLogout} className="me-2" />
-          登出
-        </CDropdownItem>
+            <CDropdownDivider />
+            <CDropdownItem href="#">
+              <CIcon icon={cilAccountLogout} className="me-2" />
+              登出
+            </CDropdownItem>
+          </>
+        ) : (
+          <>
+            <CDropdownDivider />
+            <CDropdownItem href="/#/login">
+              <CIcon icon={cilAccountLogout} className="me-2" />
+              登入
+            </CDropdownItem>
+          </>
+        )}
       </CDropdownMenu>
     </CDropdown>
   )
