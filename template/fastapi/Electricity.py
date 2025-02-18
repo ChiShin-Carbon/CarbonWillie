0224b994893d80de5fec6c13cc85ad41a0e80332
Cindy Wang <youhuawang2127cc@gmail.com>
2025-02-18 15:43:34 +0800

    Update Electricity.py

--- template/fastapi/Electricity.py
+++ template/fastapi/Electricity.py
@@ -15,7 +15,7 @@ def read_electricity():
             SELECT 
                 e.electricity_id, e.user_id, u.username, e.customer_number, e.remark, e.edit_time,
                 us.Doc_date, us.Doc_number, us.period_start, us.period_end, us.electricity_type, 
-                us.usage, us.amount, us.carbon_emission, us.remark AS usage_remark, us.img_path, us.user_id AS usage_user_id, uf.username AS usage_username, 
+                us.usage, us.amount, us.carbon_emission, us.remark AS usage_remark, us.img_path AS usage_img_path, us.user_id AS usage_user_id, uf.username AS usage_username, 
                 us.edit_time AS usage_edit_time
             FROM Electricity e
             LEFT JOIN Electricity_Usage us ON e.electricity_id = us.electricity_id
@@ -35,7 +35,7 @@ def read_electricity():
                 )
 
             electricities = defaultdict(lambda: {
-                "usage": []  # Initialize with an empty list for fill records
+                "fillrec": []  # Initialize with an empty list for fill records
             })
 
             for record in electricity_records:
@@ -50,7 +50,7 @@ def read_electricity():
                         "edit_time": record[5].strftime('%Y-%m-%d %H:%M'),
                     })
                 if record[9]:  # If there is a fill record
-                    electricities[electricity_id]["usage"].append({
+                    electricities[electricity_id]["fillrec"].append({
                         "usage_id": record[9],
                         "Doc_date": record[10],
                         "Doc_number": record[11],
@@ -59,7 +59,7 @@ def read_electricity():
                         "electricity_type": float(record[14]),
                         "usage": float(record[15]),
                         "amount": float(record[16]),
-                        "carbon_emission": record[17],
+                        "carbon_emission": float(record[17]),
                         "usage_remark": record[18],
                         "usage_img_path": record[19],
                         "usage_user_id": record[20],
