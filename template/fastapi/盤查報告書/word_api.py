9a523a34f6075fb2823072773d83d13aeb5660b5
YiTingTsai <melody920826@gmail.com>
2025-02-19 11:16:51 +0800

    修

--- template/fastapi/盤查報告書/word_api.py
+++ template/fastapi/盤查報告書/word_api.py
@@ -1,13 +1,23 @@
-from fastapi import APIRouter
+import os
+from fastapi import APIRouter, BackgroundTasks
 from fastapi.responses import FileResponse
 from 盤查報告書.word import create_word_file
 
 word_api = APIRouter(tags=["盤查報告書"])
 
+def remove_file(filename: str):
+    """ 刪除伺服器上的檔案 """
+    if os.path.exists(filename):
+        os.remove(filename)
+
 @word_api.get("/download-docx/")
-async def download_word():
+async def download_word(background_tasks: BackgroundTasks):
     filename = "test.docx"
     create_word_file(filename)  # 產生 Word 文件
+    
+    # **下載完成後，在背景刪除 Word 檔案**
+    background_tasks.add_task(remove_file, filename)
+
     return FileResponse(
         filename, 
         media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document", 
