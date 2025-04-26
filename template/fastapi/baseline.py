c000b9ce4066684dd956437a2c79202b4264606c
Cindy Wang <youhuawang2127cc@gmail.com>
2025-04-27 00:58:57 +0800

    基準年-測試中

--- template/fastapi/baseline.py
+++ template/fastapi/baseline.py
@@ -12,6 +12,30 @@ class Baseline(BaseModel):
     cfv_start_date: datetime
     cfv_end_date: datetime
 
+# 編輯基準年
+@baseline.put("/baseline/{baseline_id}")
+def update_baseline(baseline_id: int, baseline: Baseline):
+    conn = connectDB()
+    if conn:
+        cursor = conn.cursor()
+        try:
+            query = """
+                UPDATE Baseline
+                SET cfv_start_date = ?, cfv_end_date = ?, edit_time = GETDATE()
+                WHERE baseline_id = ?
+            """
+            cursor.execute(query, (baseline.cfv_start_date, baseline.cfv_end_date, baseline_id))
+            conn.commit()
+            conn.close()
+            return {"message": "Baseline updated successfully"}
+        
+        except Exception as e:
+            conn.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error updating baseline: {e}")
+    else:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Could not connect to the database.")
+
+# 新增基準年
 @baseline.post("/baseline")
 def create_baseline(baseline: Baseline):
     conn = connectDB()
@@ -33,13 +57,14 @@ def create_baseline(baseline: Baseline):
     else:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Could not connect to the database.")
 
+# 顯示基準年
 @baseline.get("/baseline")
 def read_baseline():
     conn = connectDB()
     if conn:
         cursor = conn.cursor()
         try:
-            query = "SELECT TOP 1 baseline_id, cfv_start_date, cfv_end_date, edit_time FROM Baseline ORDER BY edit_time DESC"
+            query = "SELECT TOP 1 baseline_id, cfv_start_date, cfv_end_date, edit_time, is_completed FROM Baseline ORDER BY edit_time DESC"
             cursor.execute(query)
             baseline_record = cursor.fetchone()
             conn.close()
@@ -49,7 +74,8 @@ def read_baseline():
                     "baseline_id": baseline_record[0],
                     "cfv_start_date": baseline_record[1],
                     "cfv_end_date": baseline_record[2],
-                    "edit_time": baseline_record[3]
+                    "edit_time": baseline_record[3],
+                    "is_completed": baseline_record[4]
                 }
                 return {"baseline": result}  
             else:
